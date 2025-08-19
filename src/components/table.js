a9dedd70c08e609c2b14855b6ffa621e29196c0d
Tanya <tylucsijc@yandex.ru>
2025-08-19 18:44:29 +0300

    done

--- src/components/table.js
+++ src/components/table.js
@@ -11,13 +11,35 @@ export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(template => {
+        root[template] = cloneTemplate(template);
+        root.container.prepend(root[template].container);
+    })
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    after.forEach(template => {
+        root[template] = cloneTemplate(template);
+        root.container.append(root[template].container);
+    })
 
+    root.container.addEventListener('change', () => {
+        onAction()
+    })
+    root.container.addEventListener('reset', () => {
+        setTImeout(onAction)
+    })
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault()
+        onAction(e.submitter)
+    })
+    
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate)
+            Object.keys(item).forEach(key => {
+                if (row.elements[key] && item[key] !== undefined) row.elements[key].textContent = item[key];
+            })
+            return row.container
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
